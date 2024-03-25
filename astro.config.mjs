8be91cdab31162c2f71aa8e8f04394b2f3898694
firefoxic <firefoxic.dev@gmail.com>
2024-03-25 22:06:47 +0300

    Add baseUrl option

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	base: '/dark-debris',
+	baseUrl: process.env.NODE_ENV === `development` ? `/` : `/dark-debris`,
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
